aafe2b4e0a4944bed48a6ef0d9ad23ac15608fd0
Piotr Krogulec <krogulec.piotr@gmail.com>
2019-07-01 20:17:33 +0200

    app init

---
+++ src/main/java/info/krogulec/calculator/CalculatorTaskApplication.java
@@ -0,0 +1,14 @@
+package info.krogulec.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author krogulecp
+ */
+@SpringBootApplication
+public class CalculatorTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorTaskApplication.class, args);
+    }
+}
---
+++ src/test/java/info/krogulec/calculator/CalculatorTaskApplicationTest.java
@@ -0,0 +1,18 @@
+package info.krogulec.calculator;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author krogulecp
+ */
+@SpringBootTest
+class CalculatorTaskApplicationTest {
+
+    @Test
+    void should_initialize_context(){
+
+    }
+
+}
